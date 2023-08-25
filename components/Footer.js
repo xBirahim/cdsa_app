a5b02f6dcd48ba3e4f5753e3d1f03c5a5695d2d6
AmirathBELLO <belloa@3il.fr>
2023-08-25 21:15:57 +0200

    zz

--- components/Footer.js
+++ components/Footer.js
@@ -5,17 +5,6 @@ const Footer = () => {
     <footer style={footerStyle}>
       <div style={contentContainer}>
         <p style={textStyle}>© 2023 Mon Entreprise. Tous droits réservés. | Numéro de téléphone : 123-456-789 | Email : contact@monentreprise.com</p>
-        <div style={socialIcons}>
-          <a href="https://e7.pngegg.com/pngimages/974/321/png-clipart-logo-facebook-computer-icons-instagram-facebook-desktop-wallpaper-brand.png" target="_blank" rel="noopener noreferrer">
-            <img src="facebook.png" alt="Facebook" style={iconStyle} />
-          </a>
-          <a href="https://www.instagram.com/monentreprise" target="_blank" rel="noopener noreferrer">
-            <img src="instagram.png" alt="Instagram" style={iconStyle} />
-          </a>
-          <a href="https://www.twitter.com/monentreprise" target="_blank" rel="noopener noreferrer">
-            <img src="twitter.png" alt="Twitter" style={iconStyle} />
-          </a>
-        </div>
       </div>
     </footer>
   );
